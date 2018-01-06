97e92d4735d0d91e6174830ba64e07a34b405404
CreativeGP <malicktafaminecraft@gmail.com>
2018-01-07 08:53:27 +0900

    [clean] gitignore追加

---
+++ tasks.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+"""A module written basic Fastener settings."""
+
+bacon = {
+    "scss": {
+        ".*\.scss": [ "sass #f:#p#n.css" ]
+    },
+    "sass": {
+        ".*\.sass": [ "sass #f:#p#n.css" ]
+    },
+}
